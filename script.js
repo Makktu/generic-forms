ed1c178d0563e416f5f04d3f88b8cfe6dac5694a
Makktu <johnmcna@gmail.com>
2021-10-19 20:14:33 +0100

    built the basic form in under 1 hour - progress

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 "use strict";
+
+let usersUsing = 1;
+
+const usersField = document.querySelector(".users-connected");
+
+usersField.textContent = `${usersUsing} Users Connected`;
+
+const dateField = document.querySelector(".datefield");
+
+const form = document.querySelector(".form");
+
+const newTask = document.getElementById("new-task");
+
+newTask.addEventListener("click", () => {
+    form.style.opacity = 1;
+    dateField.textContent = (new Date() + "").slice(4, -31);
+});
+
+class Task {
+    constructor(
+        name,
+        phone,
+        fromLoc,
+        toLoc,
+        details,
+        category,
+        operator,
+        dateTime
+    ) {
+        this.name = name;
+    }
+
+    printThis() {
+        // stuff
+    }
+}
